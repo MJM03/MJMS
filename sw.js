af4da96fba8b2c3c93245d4e05fbd2f0b403636e
MJM03 <mjm030798@gmail.com>
2026-07-16 05:28:38 -0500

    Delete sw.js

--- sw.js
+++
@@ -1,3 +0,0 @@
-self.addEventListener('install',()=>self.skipWaiting());
-self.addEventListener('activate',event=>event.waitUntil((async()=>{const keys=await caches.keys();await Promise.all(keys.map(k=>caches.delete(k)));await self.registration.unregister();const clientsList=await self.clients.matchAll({type:'window'});for(const client of clientsList){client.navigate(client.url);}})()));
-self.addEventListener('fetch',()=>{});
